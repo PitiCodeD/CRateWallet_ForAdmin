1b72edcff8a9c5b549066f2961eef81c9f704a92
FreePM <622707130011@dpu.ac.th>
2019-08-30 17:11:17 +0700

    Create_Splash_Screen

--- CRateWallet_ForAdmin/CRateWallet_ForAdmin.Android/MainActivity.cs
+++ CRateWallet_ForAdmin/CRateWallet_ForAdmin.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace CRateWallet_ForAdmin.Droid
 {
-    [Activity(Label = "CRateWallet_ForAdmin", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "CRateWallet_ForAdmin", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = false, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
